86fd5b368cda22817de1434e0c7206a70bdfecd3
Mikomi <50268248+syahmiharith@users.noreply.github.com>
2026-05-05 02:30:08 +0900

    Fix German low opening quote breaking

--- src/analysis.ts
+++ src/analysis.ts
@@ -222,7 +222,7 @@ export const kinsokuStart = new Set([
 export const kinsokuEnd = new Set([
   '"',
   '(', '[', '{',
-  '“', '‘', '«', '‹',
+  '“', '‘', '«', '‹', '‚', '„',
   '\uFF08',
   '\u3014',
   '\u3008',
